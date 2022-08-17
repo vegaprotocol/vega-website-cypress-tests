802e3c631cdc4f1f2e8a195898513cbabe9a340a
Oliver Gosling <goslingo@gmail.com>
2022-08-17 10:07:35 +0100

    Update page-links.spec.js
    update skipped link check as url was changed on vega.xyz

--- cypress/integration/page-links.spec.js
+++ cypress/integration/page-links.spec.js
@@ -9,7 +9,7 @@ pages.forEach((page) => {
     it('check all links in main', () => {
       cy.get("body").within(() => {
         cy.get("a").each(page => {
-          if (page.prop('href') === 'https://www.tandfonline.com/doi/pdf/10.1080/1350486X.2022.2030773?needAccess=true'){
+          if (page.prop('href') === 'https://www.tandfonline.com/doi/pdf/10.1080/1350486X.2022.2030773'){
             cy.log(`Skipping ${page.prop('href')}`);
           } else {
           cy.request(page.prop('href'));
