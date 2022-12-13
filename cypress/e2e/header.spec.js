6ae94ac6379ac98ff09eb9b62663aa4cb2bcfd39
Ben-peoplemake <ben@peoplemake.io>
2022-12-13 19:50:07 +0000

    Update header.spec.js

--- cypress/e2e/header.spec.js
+++ cypress/e2e/header.spec.js
@@ -6,5 +6,25 @@ describe("Header", () => {
     cy.wait('@getJSON').wait(300)
     cy.get('[aria-label="Switch theme color"]').trigger('click')
     cy.get('body').should('have.class', 'light')
-    });
   });
+
+  it("swap language", () => {
+      cy.server()
+      cy.route('GET', '1963098858.json').as('getJSON')
+      cy.visit("")
+      cy.wait('@getJSON').wait(300)
+      cy.get('.relative.ml-1 > .uppercase').trigger('click')
+      cy.get('.flex-col > :nth-child(2) > .text-left').trigger('click')
+      cy.get('.bg-vega-purple').should('be.visible');
+      cy.get('[aria-current="page"').should('have.attr', 'hreflang').and('include', 'es')
+  });
+
+  it("go home", () => {
+    cy.server()
+    cy.route('GET', '1963098858.json').as('getJSON')
+    cy.visit("/develop/")
+    cy.get('#header > div > div > a').click()
+    cy.wait('@getJSON').wait(300)
+    cy.url().should('eq', 'https://vega.xyz/')
+  });
+});
