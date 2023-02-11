7a79c69a8d3816a2477b87699356634b8dec9a47
Ben-peoplemake <ben@peoplemake.io>
2023-02-11 12:43:22 +0000

    Update header.spec.js

--- cypress/e2e/header.spec.js
+++ cypress/e2e/header.spec.js
@@ -1,8 +1,10 @@
 describe("Header", () => {
   it("swap themes", () => {
-    cy.server()
-    cy.route('GET', '1963098858.json').as('getJSON')
+    cy.intercept('GET', '1963098858.json').as('getJSON')
     cy.visit("")
+    cy.url().then((url) => {
+      cy.log('Current URL is: ' + url)
+    })
     cy.wait('@getJSON').wait(300)
     cy.get('[aria-label="Switch theme color"]').trigger('click')
     cy.get('body').should('have.class', 'light')
