7b229a00dc0e7fc589948103868364532d4a37cd
Ben-peoplemake <ben@peoplemake.io>
2023-03-14 12:56:26 +0000

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  screenshotOnRunFailure: false,
-  video: false,
+  screenshotOnRunFailure: true,
+  video: true,
   projectId: 'sc38ph',
   e2e: {
     // We've imported your old cypress plugins here.
@@ -10,15 +10,26 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
+    setupNodeEvents(on, config) {
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          )
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video)
+          }
+        }
+      })
+    },
     baseUrl: 'https://vega.xyz',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-     
   },
-  
     "retries": {
       // Configure retry attempts for `cypress run`
       // Default is 0
       "runMode": 1,
     }
-  
 })
