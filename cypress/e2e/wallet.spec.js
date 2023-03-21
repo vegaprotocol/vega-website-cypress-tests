0fc79a587784b706c11e83923713142b54ec80db
Ben-peoplemake <ben@peoplemake.io>
2023-03-21 13:36:11 +0000

    Update wallet.spec.js

--- cypress/e2e/wallet.spec.js
+++ cypress/e2e/wallet.spec.js
@@ -1,11 +1,13 @@
 describe("Wallet", () => {
   it("latest wallet", () => {
-    cy.intercept('GET', 'page-data.json').as('getPageData')
+    cy.intercept('GET', '/page-data/index/page-data.json').as('getPageData')
     cy.intercept('GET', 'app-data.json').as('getAppData')
     cy.visit("/wallet")
     cy.wait('@getPageData')
     cy.wait('@getAppData').then(()=>{
-    cy.get('[data-cy="Download desktop app (Mainnet)"]').click({ force: true }).click({ force: true })
+    cy.wait('@getPageData')
+    cy.scrollTo(0, -500)
+    cy.get('[data-cy="Download desktop app (Mainnet)"]').click({ force: true })
     cy.get('[data-cy="downloadLink"]').each( (item) => {
       cy.wrap(item)
         .should('have.attr', "href").and('include', 'latest')
