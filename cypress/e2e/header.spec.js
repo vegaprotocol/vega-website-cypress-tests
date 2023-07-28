e242b1ec793496e00d5e411b59354c17a3972071
bwallacee <ben@vega.xyz>
2023-07-28 09:50:20 +0100

    chore(website): update json wait

--- cypress/e2e/header.spec.js
+++ cypress/e2e/header.spec.js
@@ -1,6 +1,6 @@
 describe("Header", () => {
   it("swap themes", () => {
-    cy.intercept('GET', '/page-data/wallet/page-data.json').as('getJSON')
+    cy.intercept('GET', '/page-data/sq/d/*').as('getJSON')
     cy.visit("")
     cy.wait('@getJSON').wait(300)
     cy.get('[aria-label="Switch theme color"]').trigger('click')
