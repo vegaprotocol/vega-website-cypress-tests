9bf1141c787e4a648297124590a82df1f271e663
Edd <edd@vega.xyz>
2023-10-25 16:44:44 +0100

    try disable gpu fix

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,12 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        launchOptions.args.push('--disable-gpu')
+
+        return launchOptions
+      });
+      
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'https://vega.xyz',
