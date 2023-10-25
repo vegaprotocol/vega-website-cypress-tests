c2a06b0f028ece86b006f04418e906aa55806360
Edd <edd@vega.xyz>
2023-10-25 19:05:25 +0100

    remove unneeded arg

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,9 +2,7 @@ const { lighthouse, prepareAudit } = require('@cypress-audit/lighthouse')
 
 module.exports = (on, config) => {
   on('before:browser:launch', (browser = {}, launchOptions) => {
-    launchOptions.args.push('--disable-gpu')
     prepareAudit(launchOptions)
-    return launchOptions
   })
 
   on("task", {
