6d302d1e63142c4200c5d75ecf172c1e3397265e
Edd <edd@vega.xyz>
2024-02-12 17:46:08 +0000

    skip more substack links

--- cypress/e2e/page-links.spec.js
+++ cypress/e2e/page-links.spec.js
@@ -13,8 +13,8 @@ pages.forEach((page) => {
         cy.get("a").each(page => {
           if (page.prop('href') === 'https://www.tandfonline.com/doi/pdf/10.1080/1350486X.2022.2030773'){
             cy.log(`Skipping ${page.prop('href')}`);
-          } else if (page.prop('href') === 'https://vegacommunity.substack.com/'){
-            cy.log(`Skipping ${page.prop('href')}`);
+          } else if (page.prop('href').indexOf('substack.com/') !== -1) {
+            cy.log(`Skipping ${page.prop('href')}, substack does not like automated tests`);
           } else {
             cy.request(page.prop('href'));
           }
